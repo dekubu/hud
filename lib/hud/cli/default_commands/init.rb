e8cd15ec1e08fab8a50f242fbda487e2cedeb1d6
Delaney Burke <delaney@vidtreon.com>
2023-09-15 08:57:36 +0300

    fix the base.rb not been chnaged

--- lib/hud/cli/default_commands/init.rb
+++ lib/hud/cli/default_commands/init.rb
@@ -25,41 +25,47 @@ class Hud::CLI::DefaultCommands::Init < Hud::CLI::Command
 
   require 'fileutils'
 
-def find_replace_in_directory(directory, criteria, replacement)
-  # Recursively list all files and directories in the given directory
-  Dir.glob(File.join(directory, '**', '*')).each do |entry_path|
-    if File.file?(entry_path)
-      # For files, open the file for reading and writing
-      content = File.read(entry_path)
+  require 'fileutils'
 
-      # Create a regular expression to match the criteria as a whole word
-      regex = /\b#{Regexp.escape(criteria)}\b/i
-
-      # Check if the criteria appears in the content, case-insensitively
-      if content.match?(regex)
-        # Perform replacements based on the criteria while preserving case
-        modified_content = content.gsub(regex) do |match|
-          match == criteria ? replacement : match
+  def find_replace_in_directory(directory, criteria, replacement)
+    # Recursively list all files and directories in the given directory
+    Dir.glob(File.join(directory, '**', '*')).each do |entry_path|
+      if File.file?(entry_path)
+        # For files, open the file for reading and writing
+        content = File.read(entry_path)
+  
+        # Create a regular expression to match the criteria as a whole word
+        regex = /\b#{Regexp.escape(criteria)}\b/i
+  
+        # Check if the criteria appears in the content, case-insensitively
+        if content.match?(regex)
+          # Perform replacements based on the criteria while preserving case
+          modified_content = content.gsub(regex) do |match|
+            match == criteria ? replacement : match
+          end
+  
+          # Write the modified content back to the file
+          File.open(entry_path, 'w') { |file| file.puts modified_content }
         end
-
-        # Write the modified content back to the file
-        File.open(entry_path, 'w') { |file| file.puts modified_content }
-      end
-
-      # Check if the file name matches the criteria
-      if File.basename(entry_path).casecmp(criteria).zero?
-        new_file_name = File.join(File.dirname(entry_path), replacement)
-        File.rename(entry_path, new_file_name)
-      end
-    elsif File.directory?(entry_path)
-      # For directories, check if the directory name matches the criteria
-      if File.basename(entry_path).casecmp(criteria).zero?
-        new_directory_name = File.join(File.dirname(entry_path), replacement)
-        File.rename(entry_path, new_directory_name)
+  
+        # Check if the file name matches the criteria
+        if File.basename(entry_path).casecmp(criteria).zero?
+          new_file_name = File.join(File.dirname(entry_path), replacement)
+          File.rename(entry_path, new_file_name)
+        end
+      elsif File.directory?(entry_path)
+        # For directories, check if the directory name matches the criteria
+        if File.basename(entry_path).casecmp(criteria).zero?
+          new_directory_name = File.join(File.dirname(entry_path), replacement)
+          File.rename(entry_path, new_directory_name)
+        end
       end
     end
   end
-end
+  
+  # Example usage:
+  # find_replace_in_directory('/path/to/your/directory', 'base', 'coinbase')
+  
 
 # Example usage:
 # find_replace_in_directory('/path/to/your/directory', 'base', 'coinbase')
