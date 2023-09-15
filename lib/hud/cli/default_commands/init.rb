e38ce36a4134020aa7930867893030d8b5395086
Delaney Burke <delaney@vidtreon.com>
2023-09-15 08:36:57 +0300

    add clean up

--- lib/hud/cli/default_commands/init.rb
+++ lib/hud/cli/default_commands/init.rb
@@ -12,7 +12,9 @@ class Hud::CLI::DefaultCommands::Init < Hud::CLI::Command
     gem_root = Gem::Specification.find_by_name('hud').gem_dir
     file_path = File.join(gem_root, 'lib/hud/templates', 'base.zip')
     `unzip #{file_path} -d .`
+    `rm __MACOSX`
     find_replace_in_directory("./base", 'base', ARGV[0])
+    
 
     STDOUT.puts("Initialized #{ARGV[0]} - ok!")
     STDOUT.puts(system("tree ./#{ARGV[0]}"))
