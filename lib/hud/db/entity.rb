25d818d7677028ca33ba78ea4be7b20cf04af1c1
Delaney Burke <delaney@vidtreon.com>
2023-10-26 01:24:18 +0000

    Refactor Hud::DB::Entity to Use Module Inclusion Instead of Inheritance
    Changed the design of Hud::DB::Entity to allow it to be included as a module rather than being inherited. This means you can now include Hud::DB::Entity in your classes instead of inheriting from it.
    
    Example:
      class User
        include Hud::DB::Entity
      end
    
    Added query methods through the 'queries' block within the Entity class. These query methods are dynamically included into the automatically generated Repository class, eliminating the need for a separate Repository file.
    
    Functionalities like finding records based on attributes (find_by_name, find_by_email, etc.) should work as expected, directly from the Repository instance.
    
    Usage:
      repository = User::Repository.new
      filtered_users = repository.find_by_name("Name")

--- lib/hud/db/entity.rb
+++ lib/hud/db/entity.rb
@@ -1,84 +1,56 @@
 module Hud
-    module DB
-      class Entity
-        attr_accessor :uid,:created_at,:last_updated_at
-        def self.queries(&block)
+  module DB
+    module Entity
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def queries(&block)
           const_set(:Queries, Module.new(&block))
-          include const_get(:Queries)
+          self::Repository.include(const_get(:Queries)) if const_defined?(:Repository)
         end
-        def self.associations(repository, &block)
+
+        def associations(repository, &block)
           const_set(:AssociationMethod, Module.new(&block))
-          repository.include const_get(:AssociationMethod)
+          repository.include(const_get(:AssociationMethod))
         end
-        # Define a method to serialize an entity object to a hash
-        def to_hash
-          result = {}
-          instance_variables.each do |var_name|
-            attribute_name = var_name.to_s[1..-1].to_sym
-            result[attribute_name] = instance_variable_get(var_name)
-          end
-          result
-        end
-        def new?
-          return true if self.created_at.nil?
-          false
-        end
-        # Define a class method to create an entity object from a hash
-        def self.from_hash(uid,data_hash)
-          entity = new
-  
-          entity.uid = uid
-  
-          data_hash.each do |attribute, value|
-            # Convert the attribute name to a symbol and check if it's an attribute of the object
-            attribute = attribute.to_sym
-            if entity.respond_to?("#{attribute}=")
-              entity.send("#{attribute}=", value)
-            end
-          end
-  
-          entity
-        end
-        def self.const_missing(name)
+
+        def const_missing(name)
           @@context = self
-  
+
           if defined?(self::Queries)
-            @@queries =self::Queries
+            @@queries = self::Queries
           end
-  
+
           if name == :Repository
-            # Define the repository class dynamically
             repository_class = Class.new do
-  
-              def initialize(entity_class=@@context)
+              def initialize(entity_class = @@context)
                 @entity_class = entity_class
                 `mkdir -p ./.data`
                 @name = "./.data/#{entity_class.name}"
-  
-  
                 @entities = []
               end
-  
+
               def add(entity)
                 uid = SecureRandom.uuid
-  
                 entity.created_at = DateTime.now.to_s
                 entity.last_updated_at = entity.created_at
-  
+
                 SDBM.open(@name) do |db|
-                  db[uid] =Marshal.dump(entity.to_hash)
+                  db[uid] = Marshal.dump(entity.to_hash)
                 end
-  
+
                 uid
               end
-  
+
               def update(entity)
                 entity.last_updated_at = DateTime.now.to_s
-  
+
                 SDBM.open(@name) do |db|
-                  db.update entity.uid => Marshal.dump(entity.to_hash)
+                  db[entity.uid] = Marshal.dump(entity.to_hash)
                 end
-  
+
                 entity
               end
 
@@ -89,35 +61,33 @@ module Hud
                   update(entity)
                 end
               end
-  
+
               def delete(entity)
                 SDBM.open(@name) do |db|
                   db.delete(entity.uid)
                 end
               end
-  
+
               def reset!
-                all.map{|e| delete(e)}
+                all.map { |e| delete(e) }
               end
-  
+
               def get(uid)
-                all.find{|e| e.uid == uid}
+                all.find { |e| e.uid == uid }
               end
-  
+
               def count
                 all.count
               end
-  
+
               def all
                 result = []
-  
                 SDBM.open(@name) do |db|
                   db.each do |key, value|
                     data = Marshal.load(value)
-                    result << @entity_class.from_hash(key,data)
+                    result << @entity_class.from_hash(key, data)
                   end
                 end
-  
                 result
               end
 
@@ -125,18 +95,47 @@ module Hud
               alias :"<<" :add
               alias :save :update
             end
-  
-  
+
             if defined?(@@queries)
               repository_class.include @@queries
             end
-            # Set the Repository class as a constant within the entity class
+
             const_set(name, repository_class)
-  
           else
             super
           end
         end
       end
+
+      attr_accessor :uid, :created_at, :last_updated_at
+
+      def to_hash
+        result = {}
+        instance_variables.each do |var_name|
+          attribute_name = var_name.to_s[1..-1].to_sym
+          result[attribute_name] = instance_variable_get(var_name)
+        end
+        result
+      end
+
+      def new?
+        return true if self.created_at.nil?
+        false
+      end
+
+      def self.from_hash(uid, data_hash)
+        entity = new
+        entity.uid = uid
+
+        data_hash.each do |attribute, value|
+          attribute = attribute.to_sym
+          if entity.respond_to?("#{attribute}=")
+            entity.send("#{attribute}=", value)
+          end
+        end
+
+        entity
+      end
     end
   end
+end
