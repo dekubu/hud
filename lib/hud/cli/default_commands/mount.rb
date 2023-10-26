32aba18026d6e4ea43d46aa9c93778f9e311872d
Delaney Burke <delaney@vidtreon.com>
2023-10-26 23:31:46 +0300

    fixed the mounting

--- lib/hud/cli/default_commands/mount.rb
+++ lib/hud/cli/default_commands/mount.rb
@@ -15,9 +15,7 @@ class Hud::CLI::DefaultCommands::Mount < Hud::CLI::Command
     `unzip #{file_path} -d .`
     `rm -rf __MACOSX`
     `mv mount/ #{name}`
-
-    find_replace_in_directory("./#{name}", "base", name)
-
+    
     Dir.chdir("./#{name}") do
       rename_filename("base.rb", "#{name}.rb")
       replace_in_file("#{name}.rb", "Base", name.capitalize)
@@ -41,42 +39,6 @@ class Hud::CLI::DefaultCommands::Mount < Hud::CLI::Command
 
     # Write the modified contents back to the same file
     File.open(file_path, "w") { |file| file.puts modified_contents }
-  end
-
-  def find_replace_in_directory(directory, criteria, replacement)
-    # Recursively list all files and directories in the given directory
-    Dir.glob(File.join(directory, "**", "*")).each do |entry_path|
-      if File.file?(entry_path)
-        # For files, open the file for reading and writing
-        content = File.read(entry_path)
-
-        # Create a regular expression to match the criteria as a whole word
-        regex = /\b#{Regexp.escape(criteria)}\b/i
-
-        # Check if the criteria appears in the content, case-insensitively
-        if content.match?(regex)
-          # Perform replacements based on the criteria while preserving case
-          modified_content = content.gsub(regex) do |match|
-            (match == criteria) ? replacement : match
-          end
-
-          # Write the modified content back to the file
-          File.open(entry_path, "w") { |file| file.puts modified_content }
-        end
-
-        # Check if the file name matches the criteria
-        if File.basename(entry_path).casecmp(criteria).zero?
-          new_file_name = File.join(File.dirname(entry_path), replacement)
-          File.rename(entry_path, new_file_name)
-        end
-      elsif File.directory?(entry_path)
-        # For directories, check if the directory name matches the criteria
-        if File.basename(entry_path).casecmp(criteria).zero?
-          new_directory_name = File.join(File.dirname(entry_path), replacement)
-          File.rename(entry_path, new_directory_name)
-        end
-      end
-    end
   end
 
   def rename_filename(from, to)
